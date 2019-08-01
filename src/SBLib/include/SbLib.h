71b569897fe14083910c9aa241b9f8a133ba37b2
Jules Blok <jules.blok@gmail.com>
2019-08-02 00:38:26 +0100

    SbLib: Implement SBStr using std::string.

--- src/SBLib/include/SbLib.h
+++ src/SBLib/include/SbLib.h
@@ -1,8 +1,30 @@
 #pragma once
 
+#include <string>
 #include <vector>
 
-typedef CString SBStr;
+class SBStr
+{
+public:
+    SBStr() : m_str() {}
+    SBStr(std::string str) : m_str(str) {}
+    SBStr(const char* str) : m_str(str) {}
+
+    void SetAt(int i, char c) { m_str[i] = c; }
+    SBStr& operator=(CString& str) { m_str = str; return *this; }
+
+    SBStr Left(int nCount) { return m_str.substr(0,nCount); }
+    SBStr Mid(int i, int nCount = std::string::npos) { return m_str.substr(i,nCount); }
+    SBStr Right(int i) { return m_str.substr(m_str.size() - i, i); }
+
+    friend SBStr operator+(const SBStr& a, const SBStr& b) { return a.m_str + b.m_str; }
+
+    operator const char*() { return m_str.c_str(); }
+    operator std::string() { return m_str; }
+
+private:
+    std::string m_str;
+};
 
 template <class T>
 class SBList

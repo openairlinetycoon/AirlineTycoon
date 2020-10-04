46e8cf920f3769bc0608c44acbfdbfd81650bb15
Jules Blok <jules.blok@gmail.com>
2020-10-03 19:28:38 -0700

    defines: Redefine stricmp to strcasecmp on non-Windows platforms.

--- src/defines.h
+++ src/defines.h
@@ -1003,6 +1003,7 @@ inline bool operator!=(const CPoint& lhs, const CPoint& rhs)
 #define GetGValue(rgb) ((((WORD)(rgb)) >> 8)&0xFF)
 #define GetBValue(rgb) (((rgb)>>16)&0xFF)
 #define ZeroMemory(ptr,size) memset(ptr,0,size)
+#define stricmp strcasecmp
 
 #define FALSE 0
 #define TRUE  1
